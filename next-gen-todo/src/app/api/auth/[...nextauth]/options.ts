bf9637949b3f7ea20d0a8ae0d2e315c1bc61179f
NUBMAN <pechkursasha@gmail.com>
2024-04-05 10:28:23 +0300

    feat/configure providers in options

--- next-gen-todo/src/app/api/auth/[...nextauth]/options.ts
+++ next-gen-todo/src/app/api/auth/[...nextauth]/options.ts
@@ -1,3 +1,40 @@
 import type { NextAuthOptions } from 'next-auth'
+import Github from 'next-auth/providers/github'
+import Credentials from 'next-auth/providers/credentials'
 
-export const options: NextAuthOptions = []
+export const options: NextAuthOptions = {
+  providers: [
+    Github({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Username:',
+          type: 'text',
+          placeholder: 'your cool username',
+        },
+        password: {
+          label: 'Password',
+          type: 'text',
+          placeholder: 'Your cool username is here',
+        },
+      },
+      async authorize(credentials) {
+        // * this is where you retrieve users data
+        // * And verify with credentials
+        const user = { id: '1', name: 'bitch', password: 'pussy  ' }
+        if (
+          credentials?.username === user.name &&
+          credentials.password === user.password
+        ) {
+          return user
+        } else {
+          return null
+        }
+      },
+    }),
+  ],
+}
